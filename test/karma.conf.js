3d2f63ac8a300eb95f6f47ace7ceb8cd5682248e
Matthew Collier <mcollier@digitalbazaar.com>
2021-05-06 09:13:01 -0400

    Update to latest karma and webpack.

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -53,12 +53,6 @@ module.exports = function(config) {
     webpack: {
       devtool: 'inline-source-map',
       mode: 'development',
-      node: {
-        Buffer: false,
-        crypto: false,
-        setImmediate: false,
-        util: false
-      }
     }
   });
 };
